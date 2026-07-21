e2431437d51fd6749ca390d2e79a0383e8f6d3fa
Brundabanasahu <164716950+Brundabanasahu@users.noreply.github.com>
2026-07-21 22:52:31 +0530

    Time: 0 ms (100%), Space: 19.2 MB (51.97%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<=3:
+            return n
+
+        a,b=3,2
+        for i in range(0,n-3):
+            a=a+b
+            b=a-b
+                
+        return a
